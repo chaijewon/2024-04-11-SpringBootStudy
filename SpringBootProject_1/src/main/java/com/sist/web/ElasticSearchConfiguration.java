e9da1f77491ca93e12a54fc453d019c17deb7dcd
chaijewon <vcandjava@nate.com>
2024-10-28 12:39:51 +0900

    1111

--- SpringBootProject_1/src/main/java/com/sist/web/ElasticSearchConfiguration.java
+++ SpringBootProject_1/src/main/java/com/sist/web/ElasticSearchConfiguration.java
@@ -7,7 +7,7 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+///fbgf
 @Configuration
 public class ElasticSearchConfiguration
 {
